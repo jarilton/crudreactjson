4d090d51ec2b0696e075131edbee948314238ca0
jarilton <jariltonpereira02@hotmail.com>
2022-03-20 23:22:18 -0300

    adjusting index of crud

--- frontend/src/crud/index.js
+++ frontend/src/crud/index.js
@@ -2,7 +2,10 @@
 import React, {useEffect, useState} from 'react';
 import { toast } from 'react-toastify';
 import { getNames } from "./api";
-import {EditOutlined, deleteOutlined} from '@ant-design/icons'
+import { Link } from 'react-router-dom'
+import FormElement from './Form'
+import Loading from './Loading'
+import {EditOutlined, DeleteOutlined} from '@ant-design/icons'
 
 const Crud = () => {
   const [name, setName] = useState("");
@@ -13,20 +16,40 @@ const Crud = () => {
     loadNames();
   }, []);
 
-  const loadNames = () => getNames()
-    .then((name) => setNames(name.data));
+  const loadNames = () => getNames().then((name) => setNames(name.data));
 
     return(
       <div className="container-fluid">
         <div className="row"> 
           <div className="col-md-8">
-            {names && names.map((name) => (
-              <div className="border row mx-2 align-items-center" key={name.id}>
-                <ul className="list-group">
-                  <li className="list-group-item">{t.name}</li>
-                </ul>
-              </div>
-            ))}
+            {loading ? (
+              <Loading />
+            ) : (
+              <>
+                  <h4>Cadastro de Clientes com Json Server</h4>
+                {names && names.map((t) => (
+                  <div className="border row mx-2 align-items-center" key={t.id}>
+                    <ul className="list-group">
+                      <li className="list-group-item">{t.name}</li>
+                    </ul>
+                    <span
+                      onclick={() => console.log("")}
+                      className="btn btn-sm float-right"
+                    >
+                      <DeleteOutlined className="text-danger" />
+                    </span>
+                    <Link to={`/updates/${t.id}`}>
+                      <span
+                        onclick={() => console.log("")}
+                        className="btn btn-sm float-right"
+                      >
+                        <EditOutlined className="text-warning" />
+                      </span>
+                    </Link>
+                  </div>
+                ))}
+              </>
+            )}
           </div>
         </div>
       </div>
