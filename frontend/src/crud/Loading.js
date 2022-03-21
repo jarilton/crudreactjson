f3a6cb049ea510beb16f3043de8ccd27218be62a
jarilton <jariltonpereira02@hotmail.com>
2022-03-20 22:39:15 -0300

    feat: loading

---
+++ frontend/src/crud/Loading.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {Spin, Space} from 'antd';
+
+const Loading = () => (
+  <Space size="middle">
+    <Spin size="large" />
+  </Space>
+);
+
+export default Loading;
