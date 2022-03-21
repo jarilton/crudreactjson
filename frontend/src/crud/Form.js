ca2416003d8c4275789ebc9474a9f954fe3b68f7
jarilton <jariltonpereira02@hotmail.com>
2022-03-20 22:41:30 -0300

    feat: form

---
+++ frontend/src/crud/Form.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Input } from 'antd';
+
+const FormElement = ({ handleSubmit, name, setName }) => {
+  <form onsubmit={handleSubmit}>
+    <div className="form">
+      <Input
+        type="text"
+        placeholder="Digite o nome"
+        value={name}
+        style={{width: "50%"}}
+        autofocus
+        required
+      />
+      <br />
+      <button className="btn btn-primary mt-1">Salvar</button>
+      <button className="btn btn-danger mt-1" onClick={() => setName("")}>
+        Cancelar
+      </button>
+    </div>
+  </form>
+};
+
+export default FormElement;
